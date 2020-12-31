9df31a9514fe21ca40b8b99c6122f95231fa24b7
Esteban <e.rojas.moyano@gmail.com>
2020-12-31 00:29:35 -0300

    Nuevos Cambios

--- ProyectoAsmaGO/AsmaGOwebapp/views.py
+++ ProyectoAsmaGO/AsmaGOwebapp/views.py
@@ -1,143 +1,24 @@
-# from django.contrib import messages
-# from django.core.files.storage import FileSystemStorage
-# from django.shortcuts import render
-# from django.contrib.auth.models import User
-# from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-# from .models import Students
-# from django.contrib.auth import authenticate,login,logout
-# from django.contrib.auth.decorators import login_required
-
-# def addData(request):
-#     return render(request,"add_data.html")
-
-# def add_student(request):
-#     if request.method!="POST":
-#         return HttpResponse("<h2>Method Now Allowed</h2>")
-#     else:
-#         try:
-#             student=Students(name=request.POST.get('name',''),curso=request.POST.get('curso',''),colegio=request.POST.get('colegio',''),sintomas_diurnos=request.POST.get('sintomas_diurnos',''),medicacion=request.POST.get('medicacion',''),sintomas_nocturnos=request.POST.get('sintomas_nocturnos',''),ataques=request.POST.get('ataques',''))
-#             student.save()
-#             messages.success(request,'Agregado Exitosamente')
-#         except:
-#             messages.error(request,'Error al agregar estudiante')
-#         return HttpResponseRedirect("/addData")
-
-# def show_all_data(request):
-#     all_student=Students.objects.all()
-
-#     return render(request,"show_data.html",{'students':all_student})
-
-# def delete_student(request,student_id):
-#     student=Students.objects.get(id=student_id)
-#     student.delete()
-#     messages.error(request, "Deleted Successfully")
-#     return HttpResponseRedirect("/show_all_data")
-
-
-
-
-
-# def update_student(request,student_id):
-#     student=Students.objects.get(id=student_id)
-#     if student==None:
-#         return HttpResponse("Student Not Found")
-#     else:
-#         return render(request,"student_edit.html",{'student':student})
-
-# def edit_student(request):
-#     if request.method!="POST":
-#         return HttpResponse("<h2>Method Not Allowed</h2>")
-#     else:
-#         student=Students.objects.get(id=request.POST.get('id',''))
-#         if student==None:
-#             return HttpResponse("<h2>Student Not Found</h2>")
-#         else:
-#             student.name=request.POST.get('name','')
-#             student.curso=request.POST.get('curso','')
-#             student.colegio=request.POST.get('colegio','')
-#             student.sintomas_diurnos=request.POST.get('sintomas_diurnos','')
-#             student.medicacion=request.POST.get('medicacion','')
-#             student.sintomas_nocturnos=request.POST.get('sintomas_nocturnos','')
-#             student.ataques=request.POST.get('ataques','')
-#             student.save()
-
-#             messages.success(request,"Updated Successfully")
-#             return HttpResponseRedirect("/show_all_data")
-
-# def LoginUser(request):
-#     #print(settings.SECRET_KEY)
-#     if request.user==None or request.user =="" or request.user.username=="":
-#         return render(request,"login_page.html")
-#     else:
-#         return HttpResponseRedirect("/homePage")
-
-# def RegisterUser(request):
-#     # if request.user==None:
-#         return render(request,"register_page.html")
-#     # else:
-#         # return HttpResponseRedirect("/homePage")
-
-# def SaveUser(request):
-#     if request.method!="POST":
-#         return HttpResponse("<h2>Method Not Allowed</h2>")
-#     else:
-#         username=request.POST.get('username','')
-#         email=request.POST.get('email','')
-#         password=request.POST.get('password','')
-
-#         if not (User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists()):
-#             User.objects.create_user(username,email,password)
-#             messages.success(request,"User Created Successfully")
-#             return HttpResponseRedirect('register')
-#         else:
-#             messages.error(request,"Email or Username Already Exist")
-#             return HttpResponseRedirect('register')
-
-# def DoLoginUser(request):
-#     if request.method!="POST":
-#         return HttpResponse("<h2>Method Not Allowed")
-#     else:
-#         username=request.POST.get('username','')
-#         password=request.POST.get('password','')
-
-#         user=authenticate(username=username,password=password)
-
-#         if user!=None:
-#             login(request,user)
-#             return HttpResponseRedirect('/homePage')
-#         else:
-#             messages.error(request,"Invalid Login Details")
-#             return HttpResponseRedirect('/login_user')
-
-# def HomePage(request):
-#     return render(request,"home_page.html")
-
-# def LogoutUser(request):
-#     logout(request)
-#     request.user=None
-#     return HttpResponseRedirect("/login_user")
-
-
-# -------------------------------------------------------------------------------------------
-
 
 import json
-
-from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from AsmaGOwebapp.models import StudentData
 from django.contrib.auth.models import User
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from AsmaGOwebapp.models import StudentData
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
-from django.contrib import messages
 
 
+
+def IndexPageController(request):
+    return HttpResponseRedirect("/homePage")
+
+@login_required(login_url='/login_user/')
 def HomePage(request):
-    students=StudentData.objects.all()
-    return render(request,"home_page.html",{"students":students})
+    return render(request,"home_page.html")
 
-@csrf_exempt
+
 def InsertStudent(request):
     name=request.POST.get("name")
     curso=request.POST.get("curso")
@@ -155,7 +36,6 @@ def InsertStudent(request):
         stuent_data={"error":True,"errorMessage":"Failed to Add Student"}
         return JsonResponse(stuent_data,safe=False)
 
-@csrf_exempt
 def update_all(request):
     data=request.POST.get("data")
     dict_data=json.loads(data)
@@ -176,7 +56,7 @@ def update_all(request):
         stuent_data={"error":True,"errorMessage":"Failed to Update Data"}
         return JsonResponse(stuent_data,safe=False)
 
-@csrf_exempt
+
 def delete_data(request):
     id=request.POST.get("id")
     try:
@@ -189,7 +69,7 @@ def delete_data(request):
         return JsonResponse(stuent_data,safe=False)
 
 
-@csrf_exempt
+@login_required(login_url='/login_user/')
 def exercises(request,student_id):
     student=StudentData.objects.get(id=student_id)
     
@@ -199,11 +79,11 @@ def exercises(request,student_id):
         return HttpResponse("ola")
 
 def LoginUser(request):
-    #print(settings.SECRET_KEY)
+  
     if request.user==None or request.user =="" or request.user.username=="":
         return render(request,"login_page.html")
     else:
-        return render(request,"home_page.html")
+        return HttpResponseRedirect("/homePage") 
 
 def RegisterUser(request):
     # if request.user==None:
@@ -222,10 +102,10 @@ def SaveUser(request):
         if not (User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists()):
             User.objects.create_user(username,email,password)
             messages.success(request,"User Created Successfully")
-            return HttpResponseRedirect('register')
+            return HttpResponseRedirect('/register_user')
         else:
             messages.error(request,"Email or Username Already Exist")
-            return HttpResponseRedirect('register')
+            return HttpResponseRedirect('/register_user')
 
 def DoLoginUser(request):
     if request.method!="POST":
@@ -233,16 +113,14 @@ def DoLoginUser(request):
     else:
         username=request.POST.get('username','')
         password=request.POST.get('password','')
-
         user=authenticate(username=username,password=password)
-
+        login(request,user)
         if user!=None:
-            login(request,user)
-            return HttpResponseRedirect('')
+            
+            return HttpResponseRedirect('/homePage')
         else:
             messages.error(request,"Invalid Login Details")
             return HttpResponseRedirect('/login_user')
-
 
 def LogoutUser(request):
     logout(request)
--- ProyectoAsmaGO/ProyectoAsmaGO/urls.py
+++ ProyectoAsmaGO/ProyectoAsmaGO/urls.py
@@ -1,43 +1,8 @@
-# """ProyectoAsmaGO URL Configuration
 
-# The `urlpatterns` list routes URLs to views. For more information please see:
-#     https://docs.djangoproject.com/en/3.1/topics/http/urls/
-# Examples:
-# Function views
-#     1. Add an import:  from my_app import views
-#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-# Class-based views
-#     1. Add an import:  from other_app.views import Home
-#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-# Including another URLconf
-#     1. Import the include() function: from django.urls import include, path
-#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-# """
-# from django.contrib import admin
-# from django.urls import path, include
-# from django.conf.urls.static import static
-# from AsmaGOwebapp import views
-# from ProyectoAsmaGO import settings
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('addData',views.addData,name='add_data'),
-#     path('add_student',views.add_student, name='add_student'),
-#     path('show_all_data',views.show_all_data,name='show_all_data'),
-#     path('update_student/<str:student_id>',views.update_student,name="update_student"),
-#     path('edit_student', views.edit_student, name="edit_student"),
-#     path('delete_student/<str:student_id>', views.delete_student, name="delete_student"),
-#     path('register/',views.RegisterUser,name="register"),
-#     path('login_user/', views.LoginUser, name="login_user"),
-#     path('save_user',views.SaveUser,name="save_user"),
-#     path('do_loginn_user',views.DoLoginUser,name="do_login_user"),
-#     path('homePage/',views.HomePage,name="homepage"),
-#     path('logout/',views.LogoutUser,name="logout"),
-#     path('exercises/<str:student_id>',views.exercises,name='exercises'),
-#     ]
   
 from django.contrib import admin
-from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
 from django.conf.urls.static import static
 from AsmaGOwebapp import views
 from ProyectoAsmaGO import settings
@@ -45,14 +10,15 @@ from ProyectoAsmaGO import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomePage,name="home"),
+    path('', views.IndexPageController, name="index_page"),
+    path('homePage/',views.HomePage,name="homepage"),
     path('insert_student', views.InsertStudent,name="insert"),
     path('update_all', views.update_all,name="update_all"),
     path('delete_data', views.delete_data,name="delete_data"),
-    path('exercises/<str:student_id>',views.exercises,name='exercises'),
-    path('register/',views.RegisterUser,name="register"),
+    path('exercises/<str:student_id>/',views.exercises,name='exercises'),
+    path('register_user/',views.RegisterUser,name="register_user"),
     path('login_user/', views.LoginUser, name="login_user"),
     path('save_user',views.SaveUser,name="save_user"),
     path('do_loginn_user',views.DoLoginUser,name="do_login_user"),
     path('logout/',views.LogoutUser,name="logout"),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
